d05b7b7e1c59b7912b0b83da170845236a52412c
Daniel <daniel-ontiveros@lambdastudents.com>
2020-10-02 21:29:11 -0700

    mvp

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import {Route} from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -11,9 +13,10 @@ export default function App () {
     const getMovies = () => {
       axios
         .get('http://localhost:5000/api/movies') // Study this endpoint with Postman
-        .then(response => {
+        .then(res => {
           // Study this response with a breakpoint or log statements
           // and set the response data as the 'movieList' slice of state
+          setMovieList(res.data)
         })
         .catch(error => {
           console.error('Server Error', error);
@@ -30,7 +33,9 @@ export default function App () {
     <div>
       <SavedList list={[ /* This is stretch */]} />
 
-      <div>Replace this Div with your Routes</div>
+      <Route path='/movies/:id' component={Movie}/>
+
+      <Route path='/' render={(props) => <MovieList movies = {movieList} />}/>
     </div>
   );
 }
--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,17 +1,20 @@
 import React from 'react';
-
+import {Link} from 'react-router-dom'
 export default function MovieList(props) {
   return (
     <div className="movie-list">
       {props.movies.map(movie => (
+        <Link to={`/movies/${movie.id}`}>
         <MovieDetails key={movie.id} movie={movie} />
+        </Link>
       ))}
     </div>
   );
 }
 
 function MovieDetails(props) {
-  const { title, director, metascore } = props.movie;
+  console.log(props.movie)
+  const { title, director, metascore, id} = props.movie;
 
   return (
     <div className="movie-card">
